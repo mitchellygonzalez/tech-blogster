e70e9179c99330e50add7a9ee7e03644a9e072d3
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-10-25 21:46:56 -0400

    fixed front end a bit more

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -38,58 +38,58 @@ const postdata = [
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_body: 'http://ucla.edu/consequat/nulla.html',
+    post_body: 'Post content',
     user_id: 1
   },
   {
     title: 'Duis ac nibh.',
-    post_body: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    post_body: 'Post content',
     user_id: 9
   },
   {
     title: 'Curabitur at ipsum ac tellus semper interdum.',
-     post_body: 'https://reverbnation.com/ligula/sit.jpg',
+     post_body: 'Post content',
     user_id: 5
   },
   {
     title: 'In hac habitasse platea dictumst.',
-     post_body: 'http://china.com.cn/lectus/vestibulum.json',
+     post_body: 'Post content',
     user_id: 3
   },
   {
     title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-     post_body: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+     post_body: 'Post content',
     user_id: 10
   },
   {
     title: 'Donec dapibus.',
-     post_body: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+     post_body: 'Post content',
     user_id: 8
   },
   {
     title: 'Nulla tellus.',
-     post_body: 'https://lycos.com/natoque/penatibus/et.html',
+     post_body: 'Post content',
     user_id: 3
   },
   {
     title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-     post_body: 'https://gmpg.org/lorem.jpg',
+     post_body: 'Post content',
     user_id: 3
   },
   {
     title:
       'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-     post_body: 'https://paginegialle.it/mattis/egestas.jsp',
+     post_body: 'Post content',
     user_id: 7
   },
   {
     title: 'In hac habitasse platea dictumst.',
-     post_body: 'http://wikia.com/turpis/eget.jpg',
+     post_body: 'Post content',
     user_id: 6
   },
   {
     title: 'Etiam justo.',
-     post_body: 'https://shareasale.com/quis.json',
+     post_body: 'Post content',
     user_id: 4
   },
   {
